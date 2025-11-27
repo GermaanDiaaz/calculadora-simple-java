2d80b4b55cc83f17792bf82e2af69924094b4429
german2005motril-coder <german2005motril@gmail.com>
2025-11-27 19:35:14 +0100

    Segundo guardado

--- src/calculadora/OperacionSuma.java
+++ src/calculadora/OperacionSuma.java
@@ -1,8 +1,11 @@
 package calculadora;
 
 public class OperacionSuma {
+	  
 	    public static int sumar(int a, int b) {
+	        System.out.println("Realizando la operación de suma...");
 	        return a + b;
 	    }
+
 
 }
